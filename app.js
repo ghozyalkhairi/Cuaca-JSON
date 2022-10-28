13fe5a74bdcea24d3f14088729189529ce3c8b85
ghozyalkhairi <inighozy@gmail.com>
2022-10-29 00:18:44 +0700

    new commit

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.get("/", async (req, res) => {
 })
 
 // Setting the server to listen at port 3000
-app.listen(3000, function (req, res) {
+app.listen(process.env.PORT || 3000, function (req, res) {
   console.log("Server is running at port 3000")
 })
