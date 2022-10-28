b333dd9194674189f8b88817c5070534ce69b9e5
ghozyalkhairi <inighozy@gmail.com>
2022-10-29 00:12:52 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const app = express()
+const axios = require("axios")
+const parseString = require("xml2js").parseStringPromise
+const dataURL =
+  "https://data.bmkg.go.id/DataMKG/MEWS/DigitalForecast/DigitalForecast-Indonesia.xml"
+
+app.get("/", async (req, res) => {
+  const resp = await axios.get(dataURL)
+  const parsed = await parseString(resp.data)
+  const { forecast } = await parsed.data
+  const [{ area }] = await forecast
+  res.json(area)
+})
+
+// Setting the server to listen at port 3000
+app.listen(3000, function (req, res) {
+  console.log("Server is running at port 3000")
+})
